6ce36a9b0eb734befdf33aa7795d74b2f28a4107
Sahas-Divi <divi.sahas@gmail.com>
2026-06-10 15:03:51 +0530

    commit before moving to users branch

--- stockrecommendationapp/users/urls.py
+++ stockrecommendationapp/users/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("sign-up/", views.sign_up_view, name="sign-up"),
+    path("sign-in/", views.sign_in_view, name="sign-in"),
+    path("sign-out/", views.sign_out_view, name="sign-out")
 ]
