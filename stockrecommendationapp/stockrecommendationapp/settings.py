6671da09e1d108c869bf3fac96d3cf85cf0f0283
Sahas-Divi <divi.sahas@gmail.com>
2026-06-12 13:59:57 +0530

    updated settings.py file again

--- stockrecommendationapp/stockrecommendationapp/settings.py
+++ stockrecommendationapp/stockrecommendationapp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9c44n(7t)=mb6lxt%7i@%w)_9x#-f6v-4vy8+f94%_8kc5d930'
+load_dotenv()
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
